42bd4afc2c91cfec23f9efa9f2526ab268df05cd
fdd26 <166243616+fdd26@users.noreply.github.com>
2026-02-05 13:27:21 -0500

    align, fix typo

--- export-yahoo-finance-to-json.js
+++ export-yahoo-finance-to-json.js
@@ -1,107 +1,106 @@
 ;!(function exportYahooFinanceData()
 {
-    var filename_prefix = 'yahoo_finance_backup.';
-    var exportObj = {};
+	var filename_prefix = 'yahoo_finance_backup.';
+	var exportObj = {};
 
-    // Export LocalStorage
-    exportObj.localStorage = {};
-    var lsLen = window.localStorage && +window.localStorage.length || 0;
-    for (var li = 0; li < lsLen; ++li)
-    {
-        var lkey = window.localStorage.key(li);
-        exportObj.localStorage[lkey] = window.localStorage.getItem(lkey);
-    }
+	// Export LocalStorage
+	exportObj.localStorage = {};
+	var lsLen = window.localStorage && +window.localStorage.length || 0;
+	for (var li = 0; li < lsLen; ++li)
+	{
+		var lkey = window.localStorage.key(li);
+		exportObj.localStorage[lkey] = window.localStorage.getItem(lkey);
+	}
 
-    // Export SessionStorage
-    exportObj.sessionStorage = {};
-    var ssLen = window.sessionStorage && +window.sessionStorage.length || 0;
-    for (var si = 0; si < ssLen; ++si)
-    {
-        var skey = window.sessionStorage.key(si);
-        exportObj.sessionStorage[skey] = window.sessionStorage.getItem(skey);
-    }
+	// Export SessionStorage
+	exportObj.sessionStorage = {};
+	var ssLen = window.sessionStorage && +window.sessionStorage.length || 0;
+	for (var si = 0; si < ssLen; ++si)
+	{
+		var skey = window.sessionStorage.key(si);
+		exportObj.sessionStorage[skey] = window.sessionStorage.getItem(skey);
+	}
 
-    // Export IndexedDB
-    exportObj.indexedDB = {};
-    if (window.indexedDB && window.indexedDB.databases && window.indexedDB.open)
-    window.indexedDB.databases().then(function(dbs)
-    {
-        var dbCount = +dbs.length;
-        for (var i = 0; i < dbCount; ++i)
-        {
-            (function(dbInfo)
-             {
-                var dbName = dbInfo.name;
-                if (!dbName) return;
+	// Export IndexedDB
+	exportObj.indexedDB = {};
+	if (window.indexedDB && window.indexedDB.databases && window.indexedDB.open)
+	window.indexedDB.databases().then(function(dbs)
+	{
+		var dbCount = +dbs.length;
+		for (var i = 0; i < dbCount; ++i)
+		{
+			(function(dbInfo)
+			 {
+				var dbName = dbInfo.name;
+				if (!dbName) return;
 
-                exportObj.indexedDB[dbName] = {};
-                var dbReq = window.indexedDB.open(dbName);
-                dbReq.onsuccess = function(event)
-                {
-                    var db = event.target.result;
-                    var tx = db.transaction(db.objectStoreNames, 'readonly');
-                    var storeLen   = +db.objectStoreNames.length;
-                    var storeCount = 0;
-                    if (storeLen === 0) return;
-                    for (var j = 0; j < storeLen; ++j)
-                    {
-                        (function(storeName)
-                        {
-                            var store     = tx.objectStore(storeName);
-                            var getAllReq = store.getAll();
-                            
-                            getAllReq.onsuccess = function()
-                            {
-                                exportObj.indexedDB[dbName][storeName] = getAllReq.result;
-                                ++storeCount;
-                                if (storeCount === storeLen)
-                                {
-                                    // all stores processed for this db
-                                }
-                            };
-                            
-                            getAllReq.onerror = function()
-                            {
-                                console.warn('IndexedDB store getAll failed: ' + storeName);
-                            };
-                        })(db.objectStoreNames[j]);
-                    }
-                };
+				exportObj.indexedDB[dbName] = {};
+				var dbReq = window.indexedDB.open(dbName);
+				dbReq.onsuccess = function(event)
+				{
+					var db = event.target.result;
+					var tx = db.transaction(db.objectStoreNames, 'readonly');
+					var storeLen   = +db.objectStoreNames.length;
+					var storeCount = 0;
+					if (storeLen === 0) return;
+					for (var j = 0; j < storeLen; ++j)
+					{
+						(function(storeName)
+						{
+							var store     = tx.objectStore(storeName);
+							var getAllReq = store.getAll();
 
-                dbReq.onerror = function()
-                {
-                    console.warn('IndexedDB open failed: ' + dbName);
-                };
-            })(dbs[i]);
-        }
+							getAllReq.onsuccess = function()
+							{
+								exportObj.indexedDB[dbName][storeName] = getAllReq.result;
+								++storeCount;
+								if (storeCount === storeLen)
+								{
+									// all stores processed for this db
+								}
+							};
 
-        // Export Readable cookies (non http-only)
-        exportObj.cookies = document.cookie;
+							getAllReq.onerror = function()
+							{
+								console.warn('IndexedDB store getAll failed: ' + storeName);
+							};
+						})(db.objectStoreNames[j]);
+					}
+				};
 
-        function pad(n) { n = +n || 0; return (n < 10) ? ('0' + n) : n; }
-        
-        // Create Timestamp
-        var now     = new Date();
-        var year    = +now.getFullYear();
-        var month   = 1+now.getMonth();
-        var day     = +now.getDate();
-        var hours   = +now.getHours();
-        var minutes = +now.getMinutes();        
-        var pm      = (+hours >= 12) ? 'pm' : 'am';
-        var timestamp = [year, '-', pad(month), '-' + pad(day),
-                        '-', pad(hours), 'h', pad(minutes), pm].join('');
+				dbReq.onerror = function()
+				{
+					console.warn('IndexedDB open failed: ' + dbName);
+				};
+			})(dbs[i]);
+		}
 
-        // Download JSON file
-        var exportStr = JSON.stringify(exportObj, null, 2);
-        var blob      = new Blob([exportStr], { type: 'application/json' });
-        var url       = URL.createObjectURL(blob);
-        var filename  = filename_prefix + timestamp + '.json';
-        var a         = document.createElement('a');
-        a.href        = url;
-        a.download    = filename;
-        a.click();
-        URL.revokeObjectURL(url);
+		// Export Readable cookies (non http-only)
+		exportObj.cookies = document.cookie;
 
-        console.log('Export complete! File downloaded: ' + filename);
-    });
+		function pad(n) { n = +n || 0; return (n < 10) ? ('0' + n) : n; }
+
+		// Create Timestamp
+		var now       = new Date();
+		var year      = +now.getFullYear();
+		var month     = 1+now.getMonth();
+		var day       = +now.getDate();
+		var hours     = +now.getHours();
+		var minutes   = +now.getMinutes();
+		var pm        = (+hours >= 12) ? 'pm' : 'am';
+		var timestamp = [year, '-', pad(month), '-' + pad(day), '-', pad(hours), 'h', pad(minutes), pm].join('');
+
+		// Download JSON file
+		var exportStr = JSON.stringify(exportObj, null, 2);
+		var blob      = new Blob([exportStr], { type: 'application/json' });
+		var url       = URL.createObjectURL(blob);
+		var filename  = filename_prefix + timestamp + '.json';
+		var a         = document.createElement('a');
+		a.href        = url;
+		a.download    = filename;
+		a.click();
+		URL.revokeObjectURL(url);
+
+		console.log('Export complete! File downloaded: ' + filename);
+	});
 })();
--- import-yahoo-finance-backup.js
+++ import-yahoo-finance-backup.js
@@ -1,102 +1,103 @@
-;!(function importYahooData(json) {
-    // Parse JSON if it is a string
-    var data = json || {};
-    if (typeof json === "string")
-    {
-        data = JSON.parse(json) || {};
-    }
+;!(function importYahooFinanceData(json)
+{
+	// Parse JSON if it is a string
+	var data = json || {};
+	if (typeof json === "string")
+	{
+		data = JSON.parse(json) || {};
+	}
 
-    if (!data) { return; }
+	if (!data) { return; }
 
-    // Restore LocalStorage
-    if (data.localStorage && window.localStorage && window.localStorage.setItem)
-    {
-        for (var lkey in data.localStorage)
-        {
-            if (data.localStorage.hasOwnProperty(lkey))
-            {
-                window.localStorage.setItem(lkey, data.localStorage[lkey]);
-            }
-        }
-    }
+	// Restore LocalStorage
+	if (data.localStorage && window.localStorage && window.localStorage.setItem)
+	{
+		for (var lkey in data.localStorage)
+		{
+			if (data.localStorage.hasOwnProperty(lkey))
+			{
+				window.localStorage.setItem(lkey, data.localStorage[lkey]);
+			}
+		}
+	}
 
-    // Restore SessionStorage
-    if (data.sessionStorage && window.sessionStorage && window.sessionStorage.setItem)
-    {
-        for (var skey in data.sessionStorage)
-        {
-            if (data.sessionStorage.hasOwnProperty(skey))
-            {
-                window.sessionStorage.setItem(skey, data.sessionStorage[skey]);
-            }
-        }
-    }}
+	// Restore SessionStorage
+	if (data.sessionStorage && window.sessionStorage && window.sessionStorage.setItem)
+	{
+		for (var skey in data.sessionStorage)
+		{
+			if (data.sessionStorage.hasOwnProperty(skey))
+			{
+				window.sessionStorage.setItem(skey, data.sessionStorage[skey]);
+			}
+		}
+	}
 
-    // Restore Readable cookies (non http-only)
-    if (data.cookies)
-    {
-        var cookies = data.cookies.split("; ");
-        var len     = +cookies.length || 0;
-        for (var i = 0; i < len; ++i)
-        {
-            document.cookie = cookies[i] + "; path=/";
-        }
-    }
+	// Restore Readable cookies (non http-only)
+	if (data.cookies)
+	{
+		var cookies = data.cookies.split("; ");
+		var len     = +cookies.length || 0;
+		for (var i = 0; i < len; ++i)
+		{
+			document.cookie = cookies[i] + "; path=/";
+		}
+	}
 
-    // Restore IndexedDB
-    if (data.indexedDB && window.indexedDB && window.indexedDB.open)
-    {
-        for (var dbName in data.indexedDB)
-        {
-            if (!data.indexedDB.hasOwnProperty(dbName)) { continue; }
+	// Restore IndexedDB
+	if (data.indexedDB && window.indexedDB && window.indexedDB.open)
+	{
+		for (var dbName in data.indexedDB)
+		{
+			if (!data.indexedDB.hasOwnProperty(dbName)) { continue; }
 
-            (function(dbName)
-            {
-                var dbReq = window.indexedDB.open(dbName);
-                dbReq.onupgradeneeded = function(event)
-                {
-                    var db     = event.target.result;
-                    var stores = data.indexedDB[dbName];
-                    for (var storeName in stores)
-                    {
-                        if (!stores.hasOwnProperty(storeName)) { continue; }
-                        if (!db.objectStoreNames.contains(storeName))
-                        {
-                            db.createObjectStore(storeName, { autoIncrement: true });
-                        }
-                    }
-                };
-                
-                dbReq.onsuccess = function(event)
-                {
-                    var db = event.target.result;
-                    var tx = db.transaction(db.objectStoreNames, "readwrite");
-                    
-                    for (var storeName in data.indexedDB[dbName])
-                    {
-                        if (!data.indexedDB[dbName].hasOwnProperty(storeName)) { continue; }
-                        var store   = tx.objectStore(storeName);
-                        var records = data.indexedDB[dbName][storeName];
-                        var recLen  = +records.length || 0;
-                        for (var i = 0; i < recLen; ++i)
-                        {
-                            store.add(records[i]);
-                        }
-                    }
-                    
-                    tx.oncomplete = function()
-                    {
-                        console.log("IndexedDB restored for " + dbName);
-                    };
-                };
-                
-                dbReq.onerror = function()
-                {
-                    console.warn("Failed to open IndexedDB: " + dbName);
-                };
-            })(dbName);
-        }
-    }
+			(function(dbName)
+			{
+				var dbReq = window.indexedDB.open(dbName);
+				dbReq.onupgradeneeded = function(event)
+				{
+					var db     = event.target.result;
+					var stores = data.indexedDB[dbName];
+					for (var storeName in stores)
+					{
+						if (!stores.hasOwnProperty(storeName)) { continue; }
+						if (!db.objectStoreNames.contains(storeName))
+						{
+							db.createObjectStore(storeName, { autoIncrement: true });
+						}
+					}
+				};
 
-    console.log("Data restore attempted. Reload page to apply changes.");
+				dbReq.onsuccess = function(event)
+				{
+					var db = event.target.result;
+					var tx = db.transaction(db.objectStoreNames, "readwrite");
+
+					for (var storeName in data.indexedDB[dbName])
+					{
+						if (!data.indexedDB[dbName].hasOwnProperty(storeName)) { continue; }
+						var store   = tx.objectStore(storeName);
+						var records = data.indexedDB[dbName][storeName];
+						var recLen  = +records.length || 0;
+						for (var i = 0; i < recLen; ++i)
+						{
+							store.add(records[i]);
+						}
+					}
+
+					tx.oncomplete = function()
+					{
+						console.log("IndexedDB restored for " + dbName);
+					};
+				};
+
+				dbReq.onerror = function()
+				{
+					console.warn("Failed to open IndexedDB: " + dbName);
+				};
+			})(dbName);
+		}
+	}
+
+	console.log("Data restore attempted. Reload page to apply changes.");
 })();
